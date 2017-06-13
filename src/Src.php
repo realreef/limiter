8ab49b9264937759009931fd3efc74bd31660a79
arief syabandi <realreef@users.noreply.github.com>
2017-06-13 17:26:00 +0700

    first commit

---
+++ src/Src.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Realreef\Limiter;
+
+class Str
+{
+  /**
+  * Limit the number of words in a string.
+  *
+  * @param string $value
+  * @param int $words
+  * @param string $end
+  * @return string
+  */
+  public static function words_limiter($value, $words = 100, $end = '...')
+  {
+    preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $value, $matches);
+
+    if (! isset($matches[0]) || mb_strlen($value) === mb_strlen($matches[0])) {
+        return $value;
+    }
+
+    return rtrim($matches[0]).$end;
+  }
+}
